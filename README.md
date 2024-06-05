04fe14801d04c303659f0ca5fe5995c3a378acdc
Luigi Improta <luigi@macbookLuigi.local>
2024-06-05 14:29:23 +0200

    v.1.0

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # cancellaScreen
-qui ciao
+
+Programma che cancella gli screenshoot più vecchi di 5 minuti
+
+Accetta un solo parametro: il percorso assoluto della cartella in cui si trovano gli Screenshoot
---
+++ cancellaScreen.sh
@@ -0,0 +1,37 @@
+#!/bin/bash 
+        
+cancella()
+{    
+ 	local CONT=0
+ 	for file in /Users/luigi/Desktop/Screenshot*alle*.png  # cicla attraverso tutti i file png che contengono "Screenshot" e "alle"
+ 	do
+ 	nuovo_nome=`echo $file | sed "s/ //g"`  # elimina gli spazi dal nome del file
+ 	mv "$file" "$nuovo_nome"
+ 	file=$nuovo_nome
+ 	if [[ -f $file ]] ; then  # se esiste il file regolare  (png è regolare a quanto pare)
+ 		echo "Trovato file:" $file
+ 		oreFile=${file:24:2}  # sottostringa che parte da posizione 24 di 2 caratteri (ora di creazione)
+ 		minutiFile=${file:27:2}
+ 		secondiFile=${file:30:2}
+ 		let "creazioneFile = $oreFile*3600+$minutiFile*60+$secondiFile"
+ 		if [[ $creazioneFile -ge $(( $1-300)) ]]; then
+ 			echo "File eliminato:" $file
+ 			rm -f $file
+ 			((CONT++))
+ 		fi
+ 	fi
+ 	done
+ 	echo "File eliminati:" $CONT
+
+}
+
+ore=$(date '+%H')
+
+minuti=$(date '+%M')
+
+secondi=$(date '+%S')
+echo $ore:$minuti:$secondi
+let "avvioProgramma = $ore*3600+$minuti*60+$secondi"
+echo $avvioProgramma
+cancella $avvioProgramma
+
